81811a7f9bde4ef57d3a824fc2e1bfd45bb29982
plapis <92473629+plapis@users.noreply.github.com>
2023-02-18 16:55:17 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,6 @@
+print(". 　　　。　　　　•　 　ﾟ　　。 　　.")
+print("　　　.　　　 　　.　　　　　。　　 。　.")
+print(".　　 。　　　　　 ඞ 。 . 　　 • 　　　　•")
+print("　　ﾟ　　 Black was not An Impostor.　 。　.")
+print("　　'　　　 1 Impostor remains 　 　　。")
+print("　　ﾟ　　　.　　　. ,　　　　.　 .")
